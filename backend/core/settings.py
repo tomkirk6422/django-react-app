c585e525adfb75ce1d2d32088255b65a5985508f
Tom Kirk <tomkirk6422@gmail.com>
2025-06-18 16:14:47 -0700

    saving

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -40,16 +40,21 @@ INSTALLED_APPS = [
     "rest_framework",
     "api",
     "django_filters",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # Vite dev server
 ]
 
 ROOT_URLCONF = "core.urls"

d1393cd4e28b76d9c4e4df146484fb7c0fc7fb44
Tom Kirk <tomkirk6422@gmail.com>
2025-06-19 10:31:49 -0700

    saving

--- backend/api/views/product.py
+++ backend/api/views/product.py
@@ -9,7 +9,7 @@ from api.models.product import Product
 class ProductViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter]
-    filterset_fields = ["category__name", "tags__name"]
+    filterset_fields = ["category__name"]
     search_fields = ["description"]
 
     # overide get_queryset for re-usabilty
@@ -20,4 +20,11 @@ class ProductViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
         # Both prefetch_related and select_related prevent Django from running one query for the main objects (N),
         # and then an additional query for each object's related items (N+1 queries), which is inefficient.
         queryset = Product.objects.select_related("category").prefetch_related("tags")
+        # Im assuming the searching by multiple tags is using OR Logic, so a book only has to match 1 of 
+        # the tags in the query params to be included in the query. DjangoFilterBackend with filterset_fields
+        # only uses the last parameter when multiple tags__name values are provided, so we handle multiple tag filtering manually 
+        tag_names = self.request.query_params.getlist('tags__name')
+        if tag_names:
+             queryset = queryset.filter(tags__name__in=tag_names).distinct()
+
         return queryset
